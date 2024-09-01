b7eec6982247d5fef690628694d781f54b979f2b
Naresh Sikha <naresh.sikha1@forgeglobal.com>
2024-09-01 04:54:36 -0700

    updates

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -8,6 +8,8 @@ export default createHandler(() => (
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>Forge</title>
+          <meta name="description" content="Forge"/>
           <link rel="icon" href="/favicon.ico" />
           {assets}
         </head>
